4751e31784d5656e95289bccfade7aede375c812
abefromdublin <min903681@gmail.com>
2020-08-24 16:33:35 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Nav from './components/nav';
+import Home from './components/home';
+import About from './components/about';
+import Events from './components/events';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/events" component={Events} />
+        </Switch>
+      </BrowserRouter>
+    </Fragment>
+    
   );
 }
 
---
+++ src/components/nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../css/nav.css'
+
+function Nav() {
+    return (
+        <nav>
+            <h2>ONE.BAY</h2>
+            <ul>
+                <li><a href="/">home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/events">Events</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
