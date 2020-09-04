5939dcd599afb08edfc79af4c97bc5f308705cc1
abefromdublin <min903681@gmail.com>
2020-09-03 23:39:38 -0700

    about page changes

--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,7 @@ import '../css/about.css';
 function About() {
     return (
         <div>
-            <h1>This is about page</h1>
+            <h1>Hello Welcome to One Bay 2020</h1>
         </div>
     )
 }
