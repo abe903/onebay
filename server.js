3de8cd1d682729aa4061fb97968745cd6f82c96d
abefromdublin <min903681@gmail.com>
2020-10-10 00:06:23 -0700

    add server code

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var app = express();
+var session = require('express-session');
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
@@ -10,6 +11,11 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended : true}));
 
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -53,11 +59,21 @@ app.post('/login',(req, res) => {
     let sql = 'SELECT * FROM admin WHERE name = ? AND password = ?';
     connection.query(sql,[name, password],(err, results) => {
       if(err) throw err;
-      console.log(results)
-      res.redirect('/');
+      req.session.name = results[0].name
+      req.session.eyed = results[0].id
+      res.redirect('/admin');
     }); 
 })
 
 app.get('/signup',(req, res) => {
     res.render('signup')
+});
+
+app.get('/admin',(req, res) => {
+    let username = req.session.name; 
+    let id = req.session.eyed;
+    res.render('admin', {
+        name: username,
+        id: id
+    });
 });
